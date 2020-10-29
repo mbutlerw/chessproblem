b7399a967804dbac1c11d0d427062efdac051183
Matt Butler <mattb-w@hotmail.co.uk>
2020-10-29 18:02:31 +0000

    Update chess-problem.clj

--- chess-problem.clj
+++ chess-problem.clj
@@ -1,6 +1,3 @@
-(ns chess-problem)
-  ;; (:require [clojure.contrib.seq-utils :as s-utils]))
-
 ;; Backrow of the chess board.
 ;; Bishops are on different colours.
 ;; King has to be between the rooks.
@@ -8,19 +5,12 @@
 ;; And returns n valid backrows.
 
 (def std-backrow [:r :kn :b :ki :q :b :kn :r])
-(def inv-backrow [:r :kn :b :ki :b :q :kn :r])
-(def invk-backrow [:r :kn :b :r :b :ki :q :kn])
 
 (defn gen-backrow []
   (shuffle std-backrow))
 
-(gen-backrow)
-
 (defn gen-backrows []
   (repeatedly gen-backrow))
-
-(defn many-backrows [number]
-  (take number (filter #(and (valid-bishops %) (valid-king %)) (gen-backrows))))
 
 (defn index-of [row match]
   (keep-indexed
@@ -30,28 +20,12 @@
 (defn valid-bishops [row]
   (odd? (apply - (index-of row :b))))
 
-(valid-bishops std-backrow)
-(valid-bishops inv-backrow)
-
 (defn valid-king [row]
   (and (< (first (index-of row :r)) (first (index-of row :ki)))
-  (> (second (index-of row :r)) (first (index-of row :ki))))
-)
+  (> (second (index-of row :r)) (first (index-of row :ki)))))
 
+(vde)
+(defn many-backrows [number]
+  (take number (filter #(and (valid-bishops %) (valid-king %)) (gen-backrows))))
 
-(valid-king invk-backrow)
-
-(many-backrows 10)
-
-;; (index :a [:a :b])
-
-;; (defn indexes [index item pred]
-;;   (if (my-pred item)
-;;     index
-;;     nil))
-
-;; (defn my-pred [item]
-;;     (= :b item))
-
-;; (keep-indexed #(indexes % %2 my-pred)
-;;  std-backrow)
+(many-backrows 2)
